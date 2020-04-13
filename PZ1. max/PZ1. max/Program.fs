cdb24f0d39f511030bab8cc68208efb2103cc5c1
Unknown <arseniyzhuck@mail.ru>
2020-04-13 12:52:49 +0300

    PZ9

--- PZ1. max/PZ1. max/Program.fs
+++ PZ1. max/PZ1. max/Program.fs
@@ -5,16 +5,23 @@
 
 [<EntryPoint>]
 let main argv =
+    
+    let b = System.Convert.ToInt32(System.Console.ReadLine())
+    let c = System.Convert.ToInt32(System.Console.ReadLine())
     let p1_1 x y =
         if x>y then x
         else y 
-    
-    let b = System.Convert.ToInt32(System.Console.ReadLine())
-    let c = System.Convert.ToInt32(System.Console.ReadLine()) 
     let A = p1_1 b,c
+    System.Console.Write(A)
     
-    printfn "%A" A
+    let rec f n =
+        if n=0 then 1
+        else 
+            let n1=n-1
+            f n1
+    let f1= f 10
+    printfn "%i" f1
     let z = System.Console.ReadKey()
      
-    printfn "%A" argv
+    
     0 // return an integer exit code
