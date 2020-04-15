6b056b3d049a908ff77c1633ec4763ed862b3015
Unknown <arseniyzhuck@mail.ru>
2020-04-15 14:12:44 +0300

    9_8

--- 9_8/9_8/Program.fs
+++ 9_8/9_8/Program.fs
@@ -1,6 +1,5 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
-
 let non_del X Y = 
     if X % Y =0   then false
                   else true
@@ -9,21 +8,28 @@ let rec pros X Y =
         if X=Y then true
         else (non_del X Y) && (pros X (Y+1))
 
-
 let pr n = 
     match n with
     | 2 -> true
-    | _ -> pros n 2 
+    | _ -> pros n 2
 
-let p9_5 = 
-    let a = System.Convert.ToInt32(System.Console.ReadLine())
-    pr a
+let rec npd x y = 
+    if (x%y = 0)&&(pr y) then y
+    else npd x (y-1) 
+    
+
+let n_pd a = npd a a
+
+let p9_8 = 
+    let a= System.Convert.ToInt32(System.Console.ReadLine())
+    n_pd a
+
 
 [<EntryPoint>]
 let main argv = 
-    
-    let Ans = p9_5
+    let Ans = p9_8
     System.Console.Write(Ans)
-    let z=System.Console.ReadKey()
+    let z = System.Console.ReadKey() 
     
+    printfn "%A" argv
     0 // return an integer exit code
