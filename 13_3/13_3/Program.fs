dfe26fdc27f8b89e456a2980834a3d9cedb7fc5e
Unknown <arseniyzhuck@mail.ru>
2020-05-07 22:05:44 +0300

    LR 13

---
+++ 13_3/13_3/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+let rec read_list n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = read_list (n-1)
+    Head::Tail
+
+let read_data = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    read_list n
+
+let rec prog list=
+    match list with
+    |[] -> []
+    |h::t ->    let tail=prog t
+                if h%2=0 then h::tail
+                         else tail  
+
+let rec write_list list=
+    match list with
+    |[] ->   let z=System.Console.ReadKey()
+
+             0
+    |h::t -> System.Console.WriteLine(h.ToString())
+             write_list t   
+
+
+[<EntryPoint>]
+let main argv = 
+    write_list (prog read_data) // return an integer exit code
